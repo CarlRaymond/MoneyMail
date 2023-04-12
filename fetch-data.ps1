4abfb30c119f2e834f82f2ed72f28b8e758023c7
Carl Raymond <raymond1@msu.edu>
2023-04-12 13:56:46 -0400

    Improvements

--- fetch-data.ps1
+++ fetch-data.ps1
@@ -1,3 +1,4 @@
+New-Item -ItemType Directory -Force Data
 $zips = Get-Content .\zips.txt
 foreach ($zip in $zips) {
     Invoke-WebRequest -Uri "https://gis.usps.com/arcgis/rest/services/EDDM/selectZIP/GPServer/routes/execute?f=json&env%3AoutSR=4326&ZIP=${ZIP}&Rte_Box=R&UserName=EDDM" -OutFile ".\data\response-${ZIP}.json"
@@ -5,4 +6,5 @@ foreach ($zip in $zips) {
     jq -f .\usps2csv.jq ".\data\response-${zip}.json" > ".\data\stats-${zip}.json"
 }
 
-jq -n -f .\combine-geojson.jq .\data\routes-*.geojson > .\data\routes.geojson
+# Fails when lots of files. Command line too long.
+#jq -n -f .\combine-geojson.jq .\data\routes-*.geojson > .\data\routes.geojson
