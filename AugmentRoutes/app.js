62dd35b637654f91a7723d84d2ebb14e6e5455f1
Carl Raymond <raymond1@msu.edu>
2023-04-18 19:42:16 -0400

    Write skeleton of augment function

--- AugmentRoutes/app.js
+++ AugmentRoutes/app.js
@@ -1,15 +1,39 @@
 
-import fs from 'fs/promises';
 import zips from '../zips.json' assert { type: 'json' };
-import fetch from 'node-fetch';
+import jsonfile from 'jsonfile';
 
 let requests = [];
-let zip;
-for (zip of zips) {
-    let url = `../routes-${zip}.geojson`;
-    requests.push(fetch(url));
+
+let augment = function (data) {
+
+    for (let feature of data.features) {
+        // Each feature is a route
+        feature.properties.minDist = 0.1;
+        feature.properties.maxDist = 1.0;
+    }
+
+    return data;
 }
 
+for (let zip of zips) {
+    let inPath = `../data/routes-${zip}.geojson`;
+    let outPath = `../data/routes-augmented-${zip}.geojson`;
+    requests.push(jsonfile.readFile(inPath)
+        .then(data => {
+            if (!data.features) {
+                throw new Error(`GeoJSON file at ${inPath} does not contain features.`);
+            }
+            let adata = augment(data);
+            jsonfile.writeFile(outPath, data)
+        })
+        .catch((error) => { console.log(error); } )
+    );
+}
+
+// Wait for all results
+console.log('waiting...');
+await Promise.all(requests);
+console.log('done.');
 
 
-export { zips }
+
