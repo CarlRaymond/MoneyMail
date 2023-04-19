8accd8eafe9e77a6a95aaa6f6afe88842bef628d
Carl Raymond <raymond1@msu.edu>
2023-04-19 15:49:14 -0400

    Fetches route data from USPS
    Augements with distances. Does not yet create geojson file or stats
    file.

--- AugmentRoutes/app.js
+++ AugmentRoutes/app.js
@@ -1,6 +1,6 @@
 
-import zips from '../zips.json' assert { type: 'json' };
-import jsonfile from 'jsonfile';
+import zips from '../zips-short.json' assert { type: 'json' };
+import fs from 'fs';
 
 const METERS_PER_MILE = 1609.34;
 const RAD_PER_DEGREE = Math.PI / 180.0;
@@ -10,31 +10,27 @@ const LMN = [-84.537131, 42.703772];
 // For a GeoJSON feature, find the nearest and furthest distances from
 // the reference point.
 // Returns { min: distMeters, max: distMeters }
-function computeRouteDistances(feature, [refLon, refLat]) {
+let computeRouteDistances = function(feature, [refLon, refLat]) {
 
     // From https://stackoverflow.com/questions/43167417/calculate-distance-between-two-points-in-leaflet
-    let creflat = Math.cos(refLat * RAD_PER_DEGREE);
+    let cosRefLat = Math.cos(refLat * RAD_PER_DEGREE);
     
     // Compute min and max ref point
     let aMin = 1000000000.0, aMax = 0.0;
 
     const EARTH_RADIUS = 6378100;
-    switch (feature.geometry.type) {
-        case "MultiLineString":
-            for (let line of feature.geometry.coordinates) {
-                for (let [lon, lat] of line) {
-                    let deltaLat = Math.abs(lat - refLat) * RAD_PER_DEGREE;
-                    let deltaLon = Math.abs(lon - refLon) * RAD_PER_DEGREE;
-                    let a = Math.pow(Math.sin(deltaLat/2), 2)
-                            + creflat * Math.cos(lat * RAD_PER_DEGREE) * Math.pow(Math.sin(deltaLon/2), 2);
-                    if (a < aMin) aMin = a;
-                    if (a > aMax) aMax = a;
-                }
-            }
-        break;
+    for (let line of feature.geometry.paths) {
+        for (let [lon, lat] of line) {
+            let deltaLat = Math.abs(lat - refLat) * RAD_PER_DEGREE;
+            let deltaLon = Math.abs(lon - refLon) * RAD_PER_DEGREE;
+            let a = Math.pow(Math.sin(deltaLat/2), 2)
+                    + cosRefLat * Math.cos(lat * RAD_PER_DEGREE) * Math.pow(Math.sin(deltaLon/2), 2);
+            if (a < aMin) aMin = a;
+            if (a > aMax) aMax = a;
+        }
     }
 
-    if (aMax== 0.0) {
+    if (aMax == 0.0) {
         // No points processed
         return null;
     }
@@ -51,10 +47,10 @@ let augment = function (data) {
     for (let feature of data.features) {
         // Each feature is a route
         let d = computeRouteDistances(feature, LMN);
-        feature.properties.minDistMeters = d.minDist;
-        feature.properties.minDistMiles = d.minDist / METERS_PER_MILE;
-        feature.properties.maxDistMeters = d.maxDist;
-        feature.properties.maxDistMiles = d.maxDist / METERS_PER_MILE;
+        feature.attributes.minDistMeters = d.minDist;
+        feature.attributes.minDistMiles = d.minDist / METERS_PER_MILE;
+        feature.attributes.maxDistMeters = d.maxDist;
+        feature.attributes.maxDistMiles = d.maxDist / METERS_PER_MILE;
     }
     return data;
 }
@@ -63,17 +59,35 @@ let augment = function (data) {
 let requests = [];
 
 for (let zip of zips) {
-    let inPath = `../data/routes-${zip}.geojson`;
-    let outPath = `../data/routes-augmented-${zip}.geojson`;
-    requests.push(jsonfile.readFile(inPath)
-        .then(data => {
-            if (!data.features) {
-                throw new Error(`GeoJSON file at ${inPath} does not contain features.`);
-            }
-            let adata = augment(data);
-            jsonfile.writeFile(outPath, data)
+    let uspsUrl = `https://gis.usps.com/arcgis/rest/services/EDDM/selectZIP/GPServer/routes/execute?f=json&env%3AoutSR=4326&ZIP=${zip}&Rte_Box=R&UserName=EDDM`
+    let outPath = `../Data/data-${zip}.json`;
+
+    console.log(`Fetching route data for ${zip}...`);
+    requests.push(fetch(uspsUrl)
+        .then(response => {
+            console.log(`Received route data for ${zip}`);
+            if (!response.ok) throw new Error(`Error ${response.status}: ${response.statusText} fetching ${uspsUrl}`);
+            return response.json()
         })
-        .catch((error) => { console.log(error); } )
+        .then(data => {
+            let results = data.results;
+            if (!results) throw new Error(`Expected 'results' property not found in ${uspsUrl}`);
+
+            // If any messages, print them.
+            if (data.messages && Array.isArray(data.messages))
+                for (let m in data.messages)
+                    console.log(m);
+            
+            // Process features
+            augment(results[0].value);
+
+            // Write to file
+            return fs.promises.writeFile(outPath, JSON.stringify(data), { encoding: 'utf-8' });
+        })
+        .then(() => {
+            console.log(`Wrote ${outPath}`);
+            return;
+        })
     );
 }
 
